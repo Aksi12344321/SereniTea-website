f7b4e6b0ea36200c469f27e08e7d50cefeb375f1
Kseniya Kostromina <aksi1234@yandex.ru>
2025-05-30 12:02:02 +0300

    added header, hero section, collection section

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,31 @@
-// Галерея и лайтбоксы от Fancybox
-import { Fancybox } from '@fancyapps/ui';
-import '@fancyapps/ui/dist/fancybox/fancybox.css';
+// import Swiper bundle with all modules installed
+import Swiper from "swiper/bundle";
 
-Fancybox.bind('[data-fancybox]', {
-	// Your custom options
+// import styles bundle
+/* import "swiper/css/bundle"; */
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  slidesPerView: "auto",
+  spaceBetween: 24,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  /*   // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  }, */
 });
 
+//Закрытие тэглайна
+
+import tagline from "./modules/tagline.js";
+tagline();
 // Мобильная навигация
-import mobileNav from './modules/mobile-nav.js';
+import mobileNav from "./modules/mobile-nav.js";
 mobileNav();

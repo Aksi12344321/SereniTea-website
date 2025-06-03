4526aa8f975cdb76d477f0209f71be5e5711679a
Kseniya Kostromina <aksi1234@yandex.ru>
2025-06-03 11:31:22 +0300

    added promotion, values, newsletter sections and footer

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,11 @@
-// import Swiper bundle with all modules installed
-import Swiper from "swiper/bundle";
-
-// import styles bundle
-/* import "swiper/css/bundle"; */
-
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  slidesPerView: "auto",
-  spaceBetween: 24,
-
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-
-  /*   // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  }, */
-});
-
-//Закрытие тэглайна
-
 import tagline from "./modules/tagline.js";
 tagline();
-// Мобильная навигация
-import mobileNav from "./modules/mobile-nav.js";
-mobileNav();
+
+import swiper from "./modules/swiper.js";
+swiper();
+
+import timer from "./modules/timer.js";
+timer();
+
+// import mobileNav from './modules/mobile-nav.js';
+// mobileNav();
